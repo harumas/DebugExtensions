27920eed76c1682222f6ed42c0408511551fcaa5
Harumaro <43531665+HarumaroJP@users.noreply.github.com>
2022-06-23 11:39:02 +0900

    initial commit

---
+++ Scripts/Util/DebugUtil.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+
+public static class DebugUtil {
+    public static MethodInfo GetClearConsoleMethodInfo() {
+#if UNITY_EDITOR
+        Assembly assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        Type type = assembly
+#if UNITY_2017_1_OR_NEWER
+            .GetType("UnityEditor.LogEntries");
+#else
+           .GetType( "UnityEditorInternal.LogEntries" );
+#endif
+        MethodInfo method = type.GetMethod("Clear");
+
+        return method;
+#else
+        return null;
+#endif
+    }
+
+    public static string ToJson(object obj) {
+#if UNITY_EDITOR
+        return EditorJsonUtility.ToJson(obj, true);
+#else
+            return  JsonUtility.ToJson(obj, true);
+#endif
+    }
+}
